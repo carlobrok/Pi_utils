91597cf66d40dfa040a7eb90eb2e60c7d4dae31a
carlobrok <carlo.brokering@outlook.de>
2019-08-20 23:36:15 +0200

    first commit, pygame test

---
+++ pc/visualize_robot_log.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+
+(width, height) = (300, 200)
+screen = pygame.display.set_mode((width, height))
+pygame.display.flip()
+
+
+
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    pass
