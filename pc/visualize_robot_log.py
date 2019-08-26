266bda6436ad900ff3f96021b391c3c1b1c78b9f
carlobrok <carlo.brokering@outlook.de>
2019-08-27 00:28:35 +0200

    Added file open dialog to open .tar.gz files, added main def() and touch sen images

--- pc/visualize_robot_log.py
+++ pc/visualize_robot_log.py
@@ -1,6 +1,9 @@
 import pygame
 import time
-
+import os
+import tarfile
+import tkinter as tk
+from tkinter import filedialog
 
 def rot_center(image, angle):
     """rotate an image while keeping its center and size"""
@@ -25,68 +28,123 @@ def drop_shadow_hard_rect(screen, rect, offset, shadow_color):
         shadow_rect = (rect[0] + i, rect[1] + i, rect[2], rect[3])  # offset new rectangle x=+1, y=+1, starting with nearest rectangle
         pygame.draw.rect(screen, color, shadow_rect) # draw every shadow rect
 
+def draw_text(text, screen, position = (0,0), size = 20, color = (0, 0, 0)):
+    font = pygame.font.SysFont('opensans', size)
+    rendered_text = font.render(text, True, color)
+    screen.blit(rendered_text, position)
+
+def open_tar():
+    tk_root = tk.Tk()
+    tk_root.withdraw()
+
+    file_path = filedialog.askopenfilename(filetypes=[("Tar files", "*.tar.gz")])
+    if file_path:
+        return tarfile.open(file_path, 'r|gz'), file_path
+    else:
+        return None, None
+
+
+def main():
+
+    pygame.display.set_caption("Kamel log visualization")
+    icon = pygame.image.load(os.getcwd() + "/kamel_log_visualizer_icon.png")
+    pygame.display.set_icon(icon)
+
+
+    (width, height) = (1280, 720)
+    (robot_width, robot_height) = (360, 400)
+    (robot_x, robot_y) = (100, (height-robot_height)/2)
+
+    rect_log_button = pygame.Rect((width / 2 - 100, 20, 135, 20))
+
+    screen = pygame.display.set_mode((width, height))
+
+    light_gray = (220, 220, 220)
+    dark_gray = (100, 100, 100)
+    light_orange = (250, 141, 32)
+    light_green = (130, 222, 64)
+    background_color = light_gray
+
+
+    platte_img = pygame.image.load(os.getcwd() + "/holzplatte.jpg")
+    platte_image =  pygame.transform.scale(platte_img, (robot_width, robot_height))
+
+    dig_sen_img = pygame.image.load(os.getcwd() + "/ir_digital_sharp.jpg")
+    dig_sen_hor_l = pygame.transform.scale(dig_sen_img, (42,20))
+    dig_sen_hor_r = pygame.transform.rotate(dig_sen_hor_l, 180)
+    dig_sen_ver = pygame.transform.rotate(dig_sen_hor_l, -90)
+
+    dig_sen_offset = 5
+
+    ana_sen_img = pygame.image.load(os.getcwd() + "/ir_analog_sharp.jpg")
+    ana_sen_img.set_colorkey((255,255,255))
+    ana_sen_img = pygame.transform.scale(ana_sen_img, (74, 30))
+
+    tou_sen_img = pygame.image.load(os.getcwd() + "/endschalter.jpg")
+    tou_sen_img.set_colorkey((255,255,255))
+    tou_sen_img = pygame.transform.scale(tou_sen_img, (60, 30))
+    tou_sen_img_l = pygame.transform.rotate(tou_sen_img, 180)
+    tou_sen_img_l = pygame.transform.flip(tou_sen_img_l, True, False)
+    tou_sen_img_r = pygame.transform.rotate(tou_sen_img, 180)
+
+
+    logs_archive = None
+    logs_archive_path = ""
 
 
 
-pygame.init()
+    m_time_start = time.time()
 
-pygame.display.set_caption("Kamel log visualization")
-icon = pygame.image.load("/home/carlo/git/Pi_utils/pc/kamel_log_visualizer_icon.png")
-pygame.display.set_icon(icon)
+    running = True
+    while running:
+        m_time = time.time() - m_time_start
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if rect_log_button.collidepoint(event.pos):
+                    t_archive, t_archive_path = open_tar()
+                    if t_archive != None and logs_archive == None and t_archive_path != logs_archive_path:
+                        logs_archive = t_archive
+                        logs_archive_path = t_archive_path
+                        print("New file selected: " + logs_archive_path)
+
+        screen.fill(background_color)
+
+        #pygame.draw.rect(screen, dark_gray,(robot_x + 10, robot_y + 10, robot_width, robot_height))
+        drop_shadow_rect(screen, (robot_x, robot_y, robot_width, robot_height), 7, dark_gray, light_gray)
+        screen.blit(platte_image, (robot_x, robot_y))
+
+        screen.blit(dig_sen_hor_l, (robot_x + dig_sen_offset, robot_y + dig_sen_offset))
+        screen.blit(dig_sen_ver, (robot_x + 50 + dig_sen_offset, robot_y + dig_sen_offset))
+        screen.blit(dig_sen_ver, (robot_x + robot_width - 70 - dig_sen_offset, robot_y + dig_sen_offset))
+        screen.blit(dig_sen_hor_r, (robot_x + robot_width - 42 - dig_sen_offset, robot_y + dig_sen_offset))
+
+        screen.blit(dig_sen_hor_l, (robot_x + dig_sen_offset, robot_y + robot_height - 20 - dig_sen_offset))
+        screen.blit(dig_sen_hor_r, (robot_x + robot_width - 42 - dig_sen_offset, robot_y + robot_height - 20 - dig_sen_offset))
+
+        screen.blit(ana_sen_img, (robot_x + robot_width / 2 - 74/2, robot_y + 5))
+        screen.blit(tou_sen_img_l, (robot_x + 45, robot_y + robot_height - 18))
+        screen.blit(tou_sen_img_r, (robot_x + robot_width - 60 - 45, robot_y + robot_height - 18))
+
+        drop_shadow_rect(screen, rect_log_button, 5, dark_gray, light_gray)
+        if logs_archive == None:
+            pygame.draw.rect(screen, light_orange, rect_log_button)
+            draw_text("Select file", screen, position = (rect_log_button[0] + 15, rect_log_button[1]- 5))
+        else:
+            pygame.draw.rect(screen, light_green, rect_log_button)
+            draw_text("File selected: " , screen, position = (rect_log_button[0] + 5, rect_log_button[1] - 5))
+            draw_text(logs_archive_path, screen, size = 18, position = (rect_log_button[0] + rect_log_button[2] + 10, rect_log_button[1] - 3))
 
 
-(width, height) = (1280, 720)
-(robot_width, robot_height) = (360, 400)
-(robot_x, robot_y) = (100, (height-robot_height)/2)
-
-screen = pygame.display.set_mode((width, height))
-
-light_gray = (220, 220, 220)
-dark_gray = (100,100,100)
-background_color = light_gray
+        pygame.display.update()
+        #print(m_time)
 
 
-platte_img = pygame.image.load("/home/carlo/git/Pi_utils/pc/holzplatte.jpg")
-platte_image =  pygame.transform.scale(platte_img, (robot_width, robot_height))
-
-dig_sen_img = pygame.image.load("/home/carlo/git/Pi_utils/pc/ir_digital_sharp.jpg")
-dig_sen_hor_l = pygame.transform.scale(dig_sen_img, (42,20))
-dig_sen_hor_r = pygame.transform.rotate(dig_sen_hor_l, 180)
-dig_sen_ver = pygame.transform.rotate(dig_sen_hor_l, -90)
-
-dig_sen_offset = 5
-
-#dig_sen_image = pygame.image.load("myimage.bmp")
-#dig_sen_rect = dig_sen_image.get_rect()
-
-
-
-
-
-m_time_start = time.time()
-
-running = True
-while running:
-    m_time = time.time() - m_time_start
-
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            running = False
-            pygame.quit()
-
-    screen.fill(background_color)
-
-    #pygame.draw.rect(screen, dark_gray,(robot_x + 10, robot_y + 10, robot_width, robot_height))
-    drop_shadow_rect(screen, (robot_x, robot_y, robot_width, robot_height), 7, dark_gray, light_gray)
-    screen.blit(platte_image, (robot_x, robot_y))
-
-    screen.blit(dig_sen_hor_l, (robot_x + dig_sen_offset, robot_y + dig_sen_offset))
-    screen.blit(dig_sen_ver, (robot_x + 50 + dig_sen_offset, robot_y + dig_sen_offset))
-    screen.blit(dig_sen_ver, (robot_x + robot_width - 70 - dig_sen_offset, robot_y + dig_sen_offset))
-    screen.blit(dig_sen_hor_r, (robot_x + robot_width - 42 - dig_sen_offset, robot_y + dig_sen_offset))
-
-    screen.blit(dig_sen_hor_l, (robot_x + dig_sen_offset, robot_y + robot_height - 20 - dig_sen_offset))
-    screen.blit(dig_sen_hor_r, (robot_x + robot_width - 42 - dig_sen_offset, robot_y + robot_height - 20 - dig_sen_offset))
-
-    pygame.display.update()
-    #print(m_time)
+if __name__ == '__main__':
+    pygame.init()
+    main()
+    pygame.quit()
