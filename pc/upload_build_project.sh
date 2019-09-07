f2747a68598d67f13da3b17bdb07ef07f0d1562b
carlobrok <carlo.brokering@outlook.de>
2019-09-07 15:05:53 +0200

    Correct filepath, colored output, clean option

--- pc/upload_build_project.sh
+++ pc/upload_build_project.sh
@@ -1,15 +1,31 @@
-
 #!/bin/bash
+# Arguments: <ip> <filepath> <project subfolder on pi> [-c]
+# Example: ./upload_build_project.sh 192.168.20.95 ~/git/KamelPi/ /home/pi/projects/
+
 d=`date +%H:%M:%S`
 
-rsync --progress -ru --exclude-from "$2/.gitignore" -e ssh $2/* pi@$1:$3/$4
+project_name="$(basename $2)"
+
+#echo "ip: $1"
+#echo "path: $2"
+#echo "name: $project_name"
+#echo "destination: $3"
+
+echo "Sync project data from $2 to $1:$3$project_name"
+rsync --progress -ru --exclude-from "$2.gitignore" -e ssh $2* pi@$1:$3$project_name
 
 STATUS=$?
 if [ $STATUS -eq 0 ]; then
     echo "[$d] Successfully copied changed files!"
     echo "[$d] Rebuild project"
     echo " "
-    ssh pi@$1 "cd $3/$4/Debug; make all"
+
+    if [ "$4" = "-c" ]; then
+	echo "clean all files"
+        ssh -t pi@$1 "cd $3$project_name/Debug; make clean; make all"
+    else
+        ssh -t pi@$1 "cd $3$project_name/Debug; make all"
+    fi
 
     STATUS=$?
     if [ $STATUS -eq 0 ]; then
