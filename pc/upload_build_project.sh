75de2065bc024e4296c7e9e584eaa92202aa3228
carlobrok <carlo.brokering@outlook.de>
2020-01-08 23:49:41 +0100

    Changed Rsync options, dont print them

--- pc/upload_build_project.sh
+++ pc/upload_build_project.sh
@@ -41,7 +41,7 @@ while getopts ":huj:r" opt; do
   case ${opt} in
     h ) usage
         exit ;;
-    u ) RSYNC_OPTS=-rzvt ;;
+    u ) RSYNC_OPTS=-rzvtI ;;
     r ) REBUILD="Y" ;;
     j ) if ! [[ $OPTARG =~ ^[0-9]+$ ]] ; then
           echo "error: option -j: $OPTARG is not a number" >&2; exit 1
@@ -57,7 +57,7 @@ while getopts ":huj:r" opt; do
 done
 shift $((OPTIND -1))
 
-echo $RSYNC_OPTS
+#echo $RSYNC_OPTS
 
 if [ $# -ne 3 ]; then
   usage
@@ -90,7 +90,7 @@ rsync $RSYNC_OPTS --exclude-from="$2.gitignore" -e ssh $2* pi@$1:$3$project_name
 # if sync has been successfully (clean files and) rebuild project
 
 if [ $? -eq 0 ]; then
-  echo "Successfully copied changed files!"
+  echo "Successfully copied files!"
   echo " "
 
   # if arg -r is given clean and build, else only build
